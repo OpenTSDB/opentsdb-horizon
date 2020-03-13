d5b5557dca29d69c8a64268fb997dd5fdac2f49e
Syed Abuthahir Sait Seenipeer <syed@oath.com>
2020-03-13 08:33:03 -0700

    preventing popup close while checking heartbeat

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit, HostBinding, OnDestroy} from '@angular/core';
+import { AuthState } from './shared/state/auth.state';
+import { Observable, interval, Subscription } from 'rxjs';
+import { MatDialog} from '@angular/material';
+import { Router,  NavigationEnd } from '@angular/router';
+import { AuthService } from './core/services/auth.service';
+import { LoginExpireDialogComponent } from './core/components/login-expire-dialog/login-expire-dialog.component';
+import { Select } from '@ngxs/store';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: [ './app.component.scss' ]
+})
+export class AppComponent implements OnInit, OnDestroy {
+    @HostBinding('class.app-root') hostClass = true;
+    @Select(AuthState.getAuth) auth$: Observable<string>;
+
+    /** Local variables */
+    title = 'app';
+    fullUrlPath: string;
+    authCheckSub: Subscription;
+
+    constructor(
+        private dialog: MatDialog,
+        private router: Router,
+        private authService: AuthService
+    ) { 
+        // register this router events to capture url changes
+        this.router.events.subscribe((event) => {
+          if (event instanceof NavigationEnd) {
+            // after resolve path, this is the url the app uses
+            this.fullUrlPath = event.urlAfterRedirects;
+          }
+        });
+    }
+
+
+
+    ngOnInit() {
+        this.auth$.subscribe(auth => {
+            if (auth === 'invalid') {
+                // console.log('open auth dialog');
+                this.dialog.open(LoginExpireDialogComponent, {
+                    disableClose: true
+                });
+            } else if (auth === 'valid') {
+                this.dialog.closeAll();
+            }
+        });
+
+        const authCheck = interval(60 * 1000);
+        this.authCheckSub = authCheck.subscribe(val => {
+            return this.authService.getCookieStatus(true)
+                .subscribe(
+                        (res) => {
+                            console.log("heatbeat check res", res);
+                        }
+                );
+        });
+    }
+
+    ngOnDestroy() {
+        this.authCheckSub.unsubscribe();
+    }
+}
---
+++ frontend/src/app/core/services/auth.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Store } from '@ngxs/store';
+import { SetAuth } from '../../shared/state/auth.state';
+import { Observable , of} from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
+@Injectable()
+export class AuthService {
+    constructor(private http: HttpClient, private store: Store) {
+        //console.log('auth service store=>', this.store);
+    }
+
+    /*
+        renews the cookie.
+        image.src follows the browser redirects and renews the auth cookies
+    */
+    canCookieRenewed() {
+        const self = this;
+        return new Observable((observer) => {
+            const image = new Image();
+            image.src = '/heartbeatimg?t=' + new Date().getTime();
+            image.onload = function() {
+                observer.next('cookie-renewed');
+                self.store.dispatch(new SetAuth('valid'));
+            };
+            image.onerror = function(e) {
+                observer.next('cookie-invalid');
+                observer.complete();
+                self.store.dispatch(new SetAuth('invalid'));
+            };
+        });
+    }
+
+    /*
+        checks the login cookie. It will try to renew the cookie when the cookie is expired.
+        returns Observable<string> cookie-valid | cookie-invalid | cookie-renewed | cookie-check-error
+    */
+    getCookieStatus(heartbeat= false) {
+        const self = this;
+        self.store.dispatch(new SetAuth('unknown'));
+        return this.http.get('/heartbeat')
+            .pipe(
+                map(
+                    (res) => {
+                        if ( !heartbeat ) {
+                            self.store.dispatch(new SetAuth('valid'));
+                        }
+                        return of('cookie-valid');
+                    }
+                ),
+                catchError(
+                    error => {
+                        if ( !heartbeat && error.status === 401 ) {
+                            return this.canCookieRenewed();
+                        }
+                        return of('cookie-check-error');
+                    }
+                )
+            );
+    }
+}
