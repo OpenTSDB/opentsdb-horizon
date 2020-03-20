ad1d3045a1cedfaf9cc028003a78c6c6a25dede4
Zack Burgess <zpb210@gmail.com>
2020-03-20 15:43:03 -0700

    conversion bug fix, save alert goes back to db, form validates, recipients sorted

--- frontend/src/app/alerts/components/alert-details/alert-details.component.ts
+++ frontend/src/app/alerts/components/alert-details/alert-details.component.ts
@@ -9,7 +9,8 @@ import {
     AfterContentInit, EventEmitter,
     Output,
     Input,
-    ChangeDetectorRef
+    ChangeDetectorRef,
+    AfterViewInit
 } from '@angular/core';
 import { FormBuilder, FormGroup, FormArray, FormControl, Validators, FormsModule, NgForm } from '@angular/forms';
 import { ElementQueries, ResizeSensor} from 'css-element-queries';
@@ -50,7 +51,7 @@ import { Router } from '@angular/router';
     styleUrls: []
 })
 
-export class AlertDetailsComponent implements OnInit, OnDestroy, AfterContentInit {
+export class AlertDetailsComponent implements OnInit, OnDestroy, AfterContentInit, AfterViewInit {
     @HostBinding('class.alert-details-component') private _hostClass = true;
 
     @ViewChild('graphOutput') private graphOutput: ElementRef;
@@ -367,6 +368,12 @@ export class AlertDetailsComponent implements OnInit, OnDestroy, AfterContentIni
             };
             this.size = newSize;
         });
+    }
+
+    ngAfterViewInit() {
+        if (this.dashboardToCancelTo > 0) {
+            this.validate(false);
+        }
     }
 
     newSingleMetricTimeWindowSelected(timeInSeconds: string) {
@@ -1353,7 +1360,7 @@ export class AlertDetailsComponent implements OnInit, OnDestroy, AfterContentIni
         }
     }
 
-    validate() {
+    validate(showTopErrorBar = true) {
         this.alertForm.markAsTouched();
         switch ( this.data.type ) {
             case 'simple':
@@ -1411,8 +1418,7 @@ export class AlertDetailsComponent implements OnInit, OnDestroy, AfterContentIni
                 this.saveAlert();
             }
 
-
-        } else {
+        } else if (showTopErrorBar) {
             // set system message bar
             this.interCom.requestSend({
                 action: 'systemMessage',
@@ -1472,7 +1478,8 @@ export class AlertDetailsComponent implements OnInit, OnDestroy, AfterContentIni
         data.version = this.alertConverter.getAlertCurrentVersion();
         this.utils.setTabTitle(this.data.name);
         // emit to save the alert
-        this.configChange.emit({ action: 'SaveAlert', namespace: this.data.namespace, payload: { data: this.utils.deepClone([data]) }} );
+        this.configChange.emit({ action: 'SaveAlert', namespace: this.data.namespace, dashboard: this.dashboardToCancelTo,
+            payload: { data: this.utils.deepClone([data])}} );
     }
 
     cancelEdit() {
