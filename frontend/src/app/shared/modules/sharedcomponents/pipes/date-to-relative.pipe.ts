1a324c98b1f1f0261a455f225a910ff9b687c60c
Chris Esler <cesler@oath.com>
2020-05-05 16:38:47 -0700

    Change per Arun's suggestion to show only relative time for first week, then formatted date after that

--- frontend/src/app/shared/modules/sharedcomponents/pipes/date-to-relative.pipe.ts
+++ frontend/src/app/shared/modules/sharedcomponents/pipes/date-to-relative.pipe.ts
@@ -7,7 +7,19 @@ import * as moment from 'moment';
 })
 export class DateToRelativePipe implements PipeTransform {
 
-    transform(value: number): string {
-        return moment(value).fromNow();
+    transform(timestamp: number): string {
+        const now = moment().unix(); // current time to seconds
+        const value = moment(timestamp).unix(); // timestamp to seconds
+        const check = ((60 * 60) * 24) * 7; // 1 weeks worth of seconds
+        // const check = ((60 * 60) * 24) * 14; // 2 weeks worth of seconds
+
+        if ((now - value) < check) {
+            // if less than a week, return relative time (i.e. 2 days ago)
+            return moment(timestamp).fromNow();
+        } else {
+            // else, return formatted date (i.e. February 14th, 2020)
+            return moment(timestamp).format('MMMM Do, YYYY');
+        }
+
     }
 }
