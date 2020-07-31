ae3d31f548fd1e80b335162664e08f3cbfc0ce49
Chris Esler <cesler@oath.com>
2020-07-31 15:41:43 -0700

    updates for heatmap tooltip component

--- frontend/src/app/shared/modules/universal-data-tooltip/components/heatmap-data-tooltip/heatmap-data-tooltip.component.ts
+++ frontend/src/app/shared/modules/universal-data-tooltip/components/heatmap-data-tooltip/heatmap-data-tooltip.component.ts
@@ -6,7 +6,6 @@ import { LoggerService } from '../../../../../core/services/logger.service';
 
 import { TooltipDataService } from '../../services/tooltip-data.service';
 
-
 @Component({
     selector: 'heatmap-data-tooltip',
     templateUrl: './heatmap-data-tooltip.component.html'
@@ -14,9 +13,10 @@ import { TooltipDataService } from '../../services/tooltip-data.service';
 export class HeatmapDataTooltipComponent extends DataTooltipComponent implements OnInit, OnDestroy {
 
     @HostBinding('class.heatmap-data-tooltip') private _hostClass = true;
-    // @HostBinding('class.hidden') public tooltipHidden = true;
 
     @ViewChild('tooltipOutput', {read: ElementRef}) public ttOutputEl: ElementRef;
+
+    positionStrategy = 'sticky';
 
     constructor(
         ttDataSvc: TooltipDataService,
@@ -30,21 +30,119 @@ export class HeatmapDataTooltipComponent extends DataTooltipComponent implements
             sanitizer,
             logger
         );
-        this.logger.ng('HEATMAP CONSTRUCTOR');
     }
 
     ngOnInit() {
         super.ngOnInit();
-        super.dataStreamSubscribe((data: any) => {
+        super._dataStreamSubscribe((data: any) => {
             // this.logger.log('HEATMAP DATA CB', {data});
-            return this.dataFormatter(data);
+            const percentage = Math.ceil((100 - data.percentage)) / 100;
+            // use pSBC to get matching color value from heatmap opacity color
+            // we don't want opacity due to how tooltip color chip works
+            // so we take opacity percentage, and lighten the color
+            data.color = this.pSBC(percentage, data.color);
+            return data;
         });
     }
 
-    private dataFormatter(data: any): any {
-        // this.logger.log('HEATMAP DATA FORMATTER', data);
-        return data;
+    // utility to shade (lighten/darken), blending, linear blending
+    // modified to work with angular typescript
+    // see: https://github.com/PimpTrizkit/PJs/wiki/12.-Shade,-Blend-and-Convert-a-Web-Color-(pSBC.js)
+    // TODO: maybe add to utilsService?
+    // TODO: very useful for other types of color manipulation (blending,shading, etc)
+    pSBC(p: any, c0?: any, c1?: any, l?: any) {
+        let r: any,
+            g: any,
+            b: any,
+            P: any,
+            f: any,
+            t: any,
+            h: any;
+
+        let a: any = typeof (c1) === 'string';
+
+        const i = parseInt;
+        const m = Math.round;
+
+        if (
+            typeof (p) !== 'number' ||
+            p < -1 ||
+            p > 1 ||
+            typeof (c0) !== 'string' ||
+            (c0[0] !== 'r' && c0[0] !== '#') ||
+            (c1 && !a)
+        ) {
+            return null;
+        }
+        const pSBCr = (d) => {
+            let n = d.length;
+            const x: any = {};
+            if (n > 9) {
+                [r, g, b, a] = d = d.split(',');
+                n = d.length;
+                if (n < 3 || n > 4) {
+                    return null;
+                }
+                x.r = i(r[3] === 'a' ? r.slice(5) : r.slice(4));
+                x.g = i(g);
+                x.b = i(b);
+                x.a = a ? parseFloat(a) : -1;
+            } else {
+                if (n === 8 || n === 6 || n < 4) {
+                    return null;
+                }
+                if (n < 6) {
+                    d = '#' + d[1] + d[1] + d[2] + d[2] + d[3] + d[3] + (n > 4 ? d[4] + d[4] : '');
+                }
+                d = i(d.slice(1), 16);
+                if (n === 9 || n === 5) {
+                    // tslint:disable-next-line: no-bitwise
+                    x.r = d >> 24 & 255;
+                    // tslint:disable-next-line: no-bitwise
+                    x.g = d >> 16 & 255;
+                    // tslint:disable-next-line: no-bitwise
+                    x.b = d >> 8 & 255;
+                    // tslint:disable-next-line: no-bitwise
+                    x.a = m((d & 255) / 0.255) / 1000;
+                } else {
+                    // tslint:disable-next-line: no-bitwise
+                    x.r = d >> 16;
+                    // tslint:disable-next-line: no-bitwise
+                    x.g = d >> 8 & 255;
+                    // tslint:disable-next-line: no-bitwise
+                    x.b = d & 255;
+                    x.a = -1;
+                }
+            }
+            return x;
+        };
+        h = c0.length > 9;
+        h = a ? c1.length > 9 ? true : c1 === 'c' ? !h : false : h;
+        f = pSBCr(c0);
+        P = p < 0;
+        t = c1 && c1 !== 'c' ? pSBCr(c1) : P ? { r: 0, g: 0, b: 0, a: -1 } : { r: 255, g: 255, b: 255, a: -1 };
+        p = P ? p * -1 : p;
+        P = 1 - p;
+        if (!f || !t) {
+            return null;
+        }
+        if (l) {
+            r = m(P * f.r + p * t.r);
+            g = m(P * f.g + p * t.g);
+            b = m(P * f.b + p * t.b);
+        } else {
+            r = m((P * f.r ** 2 + p * t.r ** 2) ** 0.5);
+            g = m((P * f.g ** 2 + p * t.g ** 2) ** 0.5);
+            b = m((P * f.b ** 2 + p * t.b ** 2) ** 0.5);
+        }
+        a = f.a, t = t.a, f = a >= 0 || t >= 0, a = f ? a < 0 ? t : t < 0 ? a : a * P + t * p : 0;
+        if (h) {
+            return 'rgb' + (f ? 'a(' : '(') + r + ',' + g + ',' + b + (f ? ',' + m(a * 1000) / 1000 : '') + ')';
+        } else {
+            return '#' + (4294967296 + r * 16777216 + g * 65536 + b * 256 + (f ? m(a * 255) : 0)).toString(16).slice(1, f ? undefined : -2);
+        }
     }
+
 
     /* Last */
     ngOnDestroy() {
