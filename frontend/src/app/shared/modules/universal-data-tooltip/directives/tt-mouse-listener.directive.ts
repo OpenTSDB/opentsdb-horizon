f94491a195324af26149ad5ec400e89135068645
Chris Esler <cesler@oath.com>
2020-08-03 10:36:26 -0700

    adjustments to multigraph tooltips

--- frontend/src/app/shared/modules/universal-data-tooltip/directives/tt-mouse-listener.directive.ts
+++ frontend/src/app/shared/modules/universal-data-tooltip/directives/tt-mouse-listener.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, OnDestroy, OnInit, Input } from '@angular/core';
+import { Directive, ElementRef, OnDestroy, OnInit, Input, } from '@angular/core';
 //import { UniversalDataTooltipService } from '../services/universal-data-tooltip.service';
 import { TooltipComponentService } from '../services/tooltip-component.service';
 
@@ -9,11 +9,17 @@ import { TooltipComponentService } from '../services/tooltip-component.service';
 export class TtMouseListenerDirective implements OnDestroy, OnInit {
 
     @Input() ttType: string = 'linechart'; // line is default
+    @Input() ttMultigraph: boolean = false;
 
+    // normal mouse listener
     private _mouseEnterListener: any;
     private _mouseOutListener: any;
-
     private _mouseMoveListener: any;
+
+    // multigraph mouse listerns
+    private _mouseListeners: any[] = [];
+
+    private _mouseBoundaryEl: any;
 
     constructor(
         //private service: UniversalDataTooltipService,
@@ -23,40 +29,71 @@ export class TtMouseListenerDirective implements OnDestroy, OnInit {
         console.log('***** MOUSE LISTENER *****');
     }
 
-    ngOnInit() {
-        this._mouseEnterListener = this.elRef.nativeElement.addEventListener('mouseenter', (event: any) => {
+    ngOnInit() {}
 
+    ngAfterViewInit() {
+        this.setupMouseListener();
+    }
+
+    private setupMouseListener() {
+        //console.log('%c[[[[ SETUP MOUSE LISTENER ]]]]]', 'color: white; background: red; padding: 4px;');
+
+        this._mouseEnterListener = this.elRef.nativeElement.addEventListener('mouseenter', (event: any) => {
+            //console.log('===> ME EVENT', event);
             // find the outer boundary
             const el = this.elRef.nativeElement;
             let mBoundaryEl;
-            if (el.closest('.gridster-stage')) {
-                mBoundaryEl = el.closest('.widget-loader');
-            }
-            else if (el.closest('.edit-view-container')) {
-                mBoundaryEl = this.elRef.nativeElement;
-            }
 
-            // tell service we are entering an element that has tooltips
-            // so it can set up the correct tooltip layout
-            this.ttCompSvc.tooltipType(this.ttType, mBoundaryEl);
+            if (this.ttMultigraph) {
+                this._mouseMoveListener = this.elRef.nativeElement.addEventListener('mousemove', (e: any) => {
+                    mBoundaryEl = e.target.closest('.graph-cell');
+                    if (mBoundaryEl && this._mouseBoundaryEl !== mBoundaryEl) {
+                        this._mouseBoundaryEl = mBoundaryEl;
+                        // tell service we are entering an element that has tooltips
+                        // so it can set up the correct tooltip layout
+                        this.ttCompSvc.tooltipType(this.ttType, mBoundaryEl);
+                    }
+                }, {capture: true, passive: true});
 
+            } else {
+                if (el.closest('.gridster-stage')) {
+                    mBoundaryEl = el.closest('.widget-loader');
+                }
+                else if (el.closest('.edit-view-container')) {
+                    mBoundaryEl = this.elRef.nativeElement;
+                }
+                // tell service we are entering an element that has tooltips
+                // so it can set up the correct tooltip layout
+                this.ttCompSvc.tooltipType(this.ttType, mBoundaryEl);
+            }
         }, {capture: true, passive: true});
 
         this._mouseOutListener = this.elRef.nativeElement.addEventListener('mouseout', (event: any) => {
 
+            if (this.ttMultigraph && this._mouseMoveListener) {
+                this.elRef.nativeElement.removeEventListener('mousemove', this._mouseMoveListener)
+            }
             // tell service we are exiting an element that has tooltips
             // we just hide it, in case the next one is the same type
             this.ttCompSvc.tooltipMute();
 
         }, {capture: true, passive: true});
+
+    }
+
+    private removeListeners() {
+        //console.log('%c[[[[ REMOVE LISTENERS ]]]]]', 'color: white; background: red; padding: 4px;', this._mouseListeners);
+
+        this.elRef.nativeElement.removeEventListener('mouseenter', this._mouseEnterListener);
+        this.elRef.nativeElement.removeEventListener('mouseout', this._mouseOutListener);
+        if (this.ttMultigraph && this._mouseMoveListener) {
+            this.elRef.nativeElement.removeEventListener('mousemove', this._mouseMoveListener);
+        }
     }
 
     /* last */
     ngOnDestroy() {
         // remove events
-        if (this.elRef) {
-            this.elRef.nativeElement.removeEvent('mouseenter', this._mouseEnterListener);
-            this.elRef.nativeElement.removeEvent('mouseenter', this._mouseOutListener);
-        }
+        this.removeListeners();
     }
 }
