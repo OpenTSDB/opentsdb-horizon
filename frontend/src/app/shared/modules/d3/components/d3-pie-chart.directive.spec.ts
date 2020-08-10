a6fb8159ca12cafdd7a47bcc22261acaf355489f
Syed Abuthahir Sait Seenipeer <syed@oath.com>
2020-08-10 11:35:49 -0700

    fixing tests

--- frontend/src/app/shared/modules/d3/components/d3-pie-chart.directive.spec.ts
+++ frontend/src/app/shared/modules/d3/components/d3-pie-chart.directive.spec.ts
@@ -2,7 +2,7 @@ import { D3PieChartDirective } from './d3-pie-chart.directive';
 
 describe('D3PieChartDirective', () => {
   it('should create an instance', () => {
-    const directive = new D3PieChartDirective(null, null);
+    const directive = new D3PieChartDirective(null, null, null);
     expect(directive).toBeTruthy();
   });
 });
