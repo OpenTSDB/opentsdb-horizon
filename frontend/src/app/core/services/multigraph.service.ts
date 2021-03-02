c0182c1ae606d7520882ad5a9cf2782cb2945ff3
Hill Nguyen <hilln@verizonmedia.com>
2021-03-02 09:12:51 -0800

    add logic for checking query group or metric

--- frontend/src/app/core/services/multigraph.service.ts
+++ frontend/src/app/core/services/multigraph.service.ts
@@ -32,8 +32,9 @@ export class MultigraphService {
     return array;
   }
 
-  // multigraph by queries
-  fillMultigraphByQuery(widget: any, multiConf: any,  rawdata: any): any {
+  // fill up tag values from rawdata,multigraph by metrics
+  fillMultiTagValues(widget: any, multiConf: any, rawdata: any): any {
+    let isMultiByQuery = this.isMultiQuery(multiConf);
     const xTemp = multiConf.x ? '{{{' + Object.keys(multiConf.x).join('}}}/{{{') + '}}}' : 'x';
     const yTemp = multiConf.y ? '{{{' + Object.keys(multiConf.y).join('}}}/{{{') + '}}}' : 'y';
     let xCombine = [];
@@ -58,6 +59,7 @@ export class MultigraphService {
       const vConfig = mConfig && mConfig.settings ? mConfig.settings.visual : {};
       const dataSrc = rawdata.results[i];
       const mLabel = this.utils.getWidgetMetricDefaultLabel(widget.queries, qIndex, mIndex);
+      const gLabel = gConfig.settings.visual.label ? gConfig.settings.visual.label : '';
       if (gConfig && gConfig.settings.visual.visible && vConfig.visible) {
         if (dataSrc.source) {
           for (let j = 0; j < dataSrc.data.length; j++) {
@@ -80,14 +82,19 @@ export class MultigraphService {
             }
             // if it hasToT and expression, the metric in data return has ToT info, exclude it
             let tempMetric = !mConfig.expression ? dataSrc.data[j].metric : hasToT ? midExToT : mid;
-            const tags = { query_group: qid, ...dataSrc.data[j].tags };
+            let tags = {};
+            if (isMultiByQuery) {
+              tags = { query_group: qid + '~' + gLabel, ...dataSrc.data[j].tags };
+            } else {
+              tags = { metric_group: mConfig.id + '~' + tempMetric + alias, ...dataSrc.data[j].tags };
+            }
             let x = xTemp;
             let y = yTemp;
             // resolve multigraph with values
             const tagKeys = Object.keys(tags);
             for (let k = 0; k < tagKeys.length; k++) {
               const key = tagKeys[k];
-              const tagValue = key === 'query_group' ? tags[key] : tags[key].toLowerCase();
+              const tagValue = key === 'metric_group' || key === 'query_group' ? tags[key] : tags[key].toLowerCase();
               if (multiConf.x && x.indexOf(key) !== -1) {
                 x = x.replace('{{{' + key + '}}}', tagValue);
                 if (multiConf.x[key] && !multiConf.x[key].values.includes(tagValue)) {
@@ -181,153 +188,17 @@ export class MultigraphService {
     return results;
   }
 
-  // fill up tag values from rawdata,multigraph by metrics
-  fillMultiTagValues(widget: any, multiConf: any, rawdata: any): any {
-    const startTime = new Date().getTime();
-    const xTemp = multiConf.x ? '{{{' + Object.keys(multiConf.x).join('}}}/{{{') + '}}}' : 'x';
-    const yTemp = multiConf.y ? '{{{' + Object.keys(multiConf.y).join('}}}/{{{') + '}}}' : 'y';
-    let xCombine = [];
-    let yCombine = [];
-    let lookupData = {};
-    const results = {};
-    const regex = /\{\{([\w-.:\/]+)\}\}/ig;
-    const aliasMap = {};
-    let hasToT = false;
-    // we need to handle the case that query or metric is disable
-    for (let i = 0; i < rawdata.results.length; i++) {
-      const [source, mid] = rawdata.results[i].source.split(':');
-      const midExToT = mid.split('-')[0];
-      const tot = mid.split('-')[1];
-      hasToT = tot ? true : false;
-      const qids = this.REGDSID.exec(mid);
-      const qIndex = qids[1] ? parseInt(qids[1], 10) - 1 : 0;
-      const mIndex = this.utils.getDSIndexToMetricIndex(widget.queries[qIndex], parseInt(qids[3], 10) - 1, qids[2]);
-      const gConfig = widget.queries[qIndex] ? widget.queries[qIndex] : null;
-      const mConfig = gConfig && gConfig.metrics[mIndex] ? gConfig.metrics[mIndex] : null;
-      const vConfig = mConfig && mConfig.settings ? mConfig.settings.visual : {};
-      const dataSrc = rawdata.results[i];
-      const mLabel = this.utils.getWidgetMetricDefaultLabel(widget.queries, qIndex, mIndex);
-      if (gConfig && gConfig.settings.visual.visible && vConfig.visible) {
-        if (dataSrc.source) {
-          for (let j = 0; j < dataSrc.data.length; j++) {
-            let alias = vConfig.label === '' ? '' : '~' + vConfig.label;
-            // collecting alias marco key values
-            if (alias.trim() !== '') {
-              const matches = alias.match(regex);
-              if (matches) {
-                const mTags = { metric: !mConfig.expression ? dataSrc.data[j].metric : mLabel, ...dataSrc.data[j].tags };
-                for (let i = 0, len = matches.length; i < len; i++) {
-                  const key = matches[i].replace(/\{|\}/g, '');
-                  if (mTags[key]) {
-                    if (!aliasMap[key]) { aliasMap[key] = []; }
-                    if (!aliasMap[key].includes(mTags[key])) {
-                      aliasMap[key].push(mTags[key]);
-                    }
-                  }
-                }
-              }
-            }
-            // if it hasToT and expression, the metric in data return has ToT info, exclude it
-            let tempMetric = !mConfig.expression ? dataSrc.data[j].metric : hasToT ? midExToT : mid;
-            const tags = { metric_group: mConfig.id + '~' + tempMetric + alias, ...dataSrc.data[j].tags };
-            let x = xTemp;
-            let y = yTemp;
-            // resolve multigraph with values
-            const tagKeys = Object.keys(tags);
-            for (let k = 0; k < tagKeys.length; k++) {
-              const key = tagKeys[k];
-              const tagValue = key === 'metric_group' ? tags[key] : tags[key].toLowerCase();
-              if (multiConf.x && x.indexOf(key) !== -1) {
-                x = x.replace('{{{' + key + '}}}', tagValue);
-                if (multiConf.x[key] && !multiConf.x[key].values.includes(tagValue)) {
-                  multiConf.x[key].values.push(tagValue);
-                }
-              }
-              if (multiConf.y && y.indexOf(key) !== -1) {
-                y = y.replace('{{{' + key + '}}}', tagValue);
-                if (multiConf.y[key] && !multiConf.y[key].values.includes(tagValue)) {
-                  multiConf.y[key].values.push(tagValue);
-                }
-              }
-            }
-            if (!lookupData[y]) {
-              lookupData[y] = {};
-            }
-            if (!lookupData[y][x]) {
-              lookupData[y][x] = {
-                results: []
-              };
-            }
-            let srcIndex = lookupData[y][x].results.findIndex(d => d.source === dataSrc.source);
-            if (srcIndex === -1) {
-              lookupData[y][x].results.push({
-                source: dataSrc.source,
-                timeSpecification: dataSrc.timeSpecification,
-                data: []
-              });
-              srcIndex = lookupData[y][x].results.length - 1;
-            }
-            lookupData[y][x].results[srcIndex].data.push(dataSrc.data[j]);
-          }
-        }
+  // check if it's query or metric group
+  isMultiQuery(multiConf: any): boolean {
+    let isQueryGroup = false;
+    const keys = Object.keys(multiConf);
+    for (let i = 0; i < keys.length; i++) {
+      if (multiConf[keys[i]].hasOwnProperty('query_group')) {
+        isQueryGroup = true;
+        break;
       }
     }
-    // turn array to string and slice them
-    let aliasObj = {};
-    if (Object.keys(aliasMap).length > 0) {
-      for (let key in aliasMap) {
-        aliasObj[key] = aliasMap[key].sort().join('');
-        aliasObj[key] = aliasObj[key].length > 20 ? aliasObj[key].slice(0, 20) + '...' : aliasObj[key];
-      }
-      let lookupStr = JSON.stringify(lookupData);
-      let multiConfStr = JSON.stringify(multiConf);
-      for (let key in aliasObj) {
-        let _regex = new RegExp('{{' + key + '}}', 'gi');
-        lookupStr = lookupStr.replace(_regex, aliasObj[key]);
-        multiConfStr = multiConfStr.replace(_regex, aliasObj[key]);
-      }
-      lookupData = JSON.parse(lookupStr);
-      multiConf = JSON.parse(multiConfStr);
-    }
-    // let build the master results table
-    const xAll = multiConf.x ? [] : [['x']];
-    const yAll = multiConf.y ? [] : [['y']];
-    for (const tag in multiConf.x) {
-      if (multiConf.x.hasOwnProperty(tag)) {
-        multiConf.x[tag].values.sort(this.utils.sortAliasforMultigraph(multiConf.x[tag].sortAs));
-        xAll.push(multiConf.x[tag].values);
-      }
-    }
-    for (const tag in multiConf.y) {
-      if (multiConf.y.hasOwnProperty(tag)) {
-        multiConf.y[tag].values.sort(this.utils.sortAliasforMultigraph(multiConf.y[tag].sortAs));
-        yAll.push(multiConf.y[tag].values);
-      }
-    }
-    // apply sort for those keys based on con
-    for (let i = 0; i < xAll.length; i++) {
-      xCombine = this.combineKeys(xCombine, xAll[i]);
-    }
-    for (let i = 0; i < yAll.length; i++) {
-      yCombine = this.combineKeys(yCombine, yAll[i]);
-    }
-    for (let i = 0; i < yCombine.length; i++) {
-      for (let j = 0; j < xCombine.length; j++) {
-        if ((multiConf.layout === 'grid' || lookupData[yCombine[i]]) && !results[yCombine[i]]) {
-          results[yCombine[i]] = {};
-        }
-        if (!results[yCombine[i]][xCombine[j]] && multiConf.layout === 'grid') {
-          results[yCombine[i]][xCombine[j]] = {};
-        }
-        if (lookupData[yCombine[i]] && lookupData[yCombine[i]][xCombine[j]]) {
-          results[yCombine[i]][xCombine[j]] = lookupData[yCombine[i]][xCombine[j]];
-        }
-      }
-    }
-    if (!Object.keys(lookupData).length) {
-      results['y'] = { 'x': rawdata };
-    }
-    return results;
+    return isQueryGroup;
   }
 
   // build multigraph config
