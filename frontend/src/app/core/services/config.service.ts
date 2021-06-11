177b5e534f18d91eddea4a41a3d1b2d19ecfbee2
Syed Abuthahir Sait Seenipeer <syed@oath.com>
2021-06-11 10:04:40 -0700

    validation on endpoints, app config update

--- frontend/src/app/core/services/config.service.ts
+++ frontend/src/app/core/services/config.service.ts
@@ -7,15 +7,30 @@ import { HttpClient } from '@angular/common/http';
 export class AppConfigService {
   private appConfig;
   public initialized;
+  public errors = [];
 
   constructor(private http: HttpClient) { }
 
   loadAppConfig() {
     return this.http.get('/config')
       .toPromise()
-      .then(data => {
-        this.appConfig = data;
+      .then( (data: any) => {
         this.initialized = true;
+        if ( !data.tsdb_host && ( !data.tsdb_hosts || !data.tsdb_hosts.length ) ) {
+          this.errors.push("TSDB endpoint is invalid");
+        }
+        if ( !data.configdb ) {
+          this.errors.push("Configdb endpoint is invalid");
+        }
+        if ( !data.metaApi ) {
+          this.errors.push("Meta endpoint is invalid");
+        }
+        if ( !data.auraUI ) {
+          this.errors.push("Aura UI endpoint is invalid");
+        }
+        if ( !this.errors.length ) {
+          this.appConfig = data;
+        }
       })
       .catch(error => {
         this.initialized = false;
