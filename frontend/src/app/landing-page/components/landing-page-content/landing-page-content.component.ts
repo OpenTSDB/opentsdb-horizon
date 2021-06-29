47bea7e861c43a5abf1c74b7a02d12a460d7b678
Syed Abuthahir Sait Seenipeer <syed@oath.com>
2021-06-29 12:34:10 -0700

    config: adding app name

--- frontend/src/app/landing-page/components/landing-page-content/landing-page-content.component.ts
+++ frontend/src/app/landing-page/components/landing-page-content/landing-page-content.component.ts
@@ -4,6 +4,7 @@ import { Subscription, Observable } from 'rxjs';
 import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { IntercomService } from '../../../core/services/intercom.service';
 import { MatSnackBar } from '@angular/material';
+import { AppConfigService } from '../../../core/services/config.service';
 
 import {
     Select,
@@ -105,6 +106,8 @@ export class LandingPageContentComponent implements OnInit, OnDestroy {
     /** Form Group */
     searchFormGroup: FormGroup;
 
+    appName;
+
     constructor(
         // private dbService: DashboardService,
         private interCom: IntercomService,
@@ -113,10 +116,12 @@ export class LandingPageContentComponent implements OnInit, OnDestroy {
         private fb: FormBuilder,
         private snackBar: MatSnackBar,
         private store: Store,
-        private console: ConsoleService
+        private console: ConsoleService,
+        private appConfig: AppConfigService
     ) { }
 
     ngOnInit() {
+        this.appName = this.appConfig.getConfig().name;
         this.createSearchForm();
         this.subscription.add(this.route.queryParams.subscribe(params => {
             if (params['db-delete']) {
