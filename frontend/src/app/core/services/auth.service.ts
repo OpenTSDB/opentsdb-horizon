9082568c99768cfd73f042bac8db56f8d8d09ad6
Syed Abuthahir Sait Seenipeer <syed@oath.com>
2021-10-01 12:46:06 -0700

    resolving conflict

--- frontend/src/app/core/services/auth.service.ts
+++ frontend/src/app/core/services/auth.service.ts
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Store } from '@ngxs/store';
