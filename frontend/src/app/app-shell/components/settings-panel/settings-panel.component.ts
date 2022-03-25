1eb95e53e790f84d7cadf5d600e80f75757f5061
Chris Esler <cesler@oath.com>
2022-03-25 11:57:54 -0700

    updating angular cli and core to 8

--- frontend/src/app/app-shell/components/settings-panel/settings-panel.component.ts
+++ frontend/src/app/app-shell/components/settings-panel/settings-panel.component.ts
@@ -39,7 +39,7 @@ import { NavigatorPanelComponent } from '../navigator-panel/navigator-panel.comp
 export class SettingsPanelComponent implements OnInit {
     //@HostBinding('class.settings-navigator') private _hostClass = true;
 
-    @ViewChild(NavigatorPanelComponent) private navPanel: NavigatorPanelComponent;
+    @ViewChild(NavigatorPanelComponent, { static: false }) private navPanel: NavigatorPanelComponent;
 
     @Input() activeNavSection: any = '';
     @Input() drawerMode: any = 'over';
