1ec3b865f6794cd16c872457b7c927124a48ae53
Chris Esler <cesler@oath.com>
2022-03-28 12:07:04 -0700

    fixes for update to angular 8

--- frontend/src/app/shared/dygraphs/plotters/index.js
+++ frontend/src/app/shared/dygraphs/plotters/index.js
@@ -14,8 +14,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+barChartPlotter = require('./barchart.js');
+multiColumnBarPlotter = require('./multicolumbarchart');
+heatmapPlotter = require('./heatmap.js')
+stackedAreaPlotter = require('./stackedarea.js');
 
-module.exports.barChartPlotter = require('./barchart.js');
-module.exports.multiColumnBarPlotter = require('./multicolumbarchart');
-module.exports.heatmapPlotter = require('./heatmap.js');
-module.exports.stackedAreaPlotter = require('./stackedarea.js');
+module.exports = { barChartPlotter, heatmapPlotter, stackedAreaPlotter };
