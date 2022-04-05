e3fda7a932f6c0e53decf9ba66a0bb4ec1a1a824
Chris Esler <cesler@oath.com>
2022-04-05 11:58:42 -0700

    last commit to get angular updated to version 9

--- frontend/src/app/shared/dygraphs/plotters/stackedarea.js
+++ frontend/src/app/shared/dygraphs/plotters/stackedarea.js
@@ -21,7 +21,7 @@ var utils = require("dygraphs/src/dygraph-utils");
 
 function stackedAreaPlotter(e) {
     //extracting and reducing the Dygraph.stackPoints_ function
-    var stackPoints = function (
+    stackPoints = function (
         points,
         cumulativeYval,
         seriesExtremes,
@@ -435,9 +435,13 @@ var DygraphLayoutCalcYNormal_ = function (axis, value, logscale) {
  * connectSeparatedPoints is true. When it's false, the predicate will
  * skip over points with missing yVals.
  */
+<<<<<<< HEAD
 var DygraphCanvasRendererGetIteratorPredicate = function (
     connectSeparatedPoints
 ) {
+=======
+var DygraphCanvasRendererGetIteratorPredicate = function (connectSeparatedPoints) {
+>>>>>>> d2529e5f (last commit to get angular updated to version 9)
     return connectSeparatedPoints
         ? DygraphCanvasRendererPredicateThatSkipsEmptyPoints
         : null;
