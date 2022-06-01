14d7e436ffdf304eee8966e89db4c44533f52dfa
Segawa Lucassuzuki <lsegawa@yahoo-corp.jp>
2022-06-01 21:09:07 +0900

    fixed the offending bug

--- frontend/src/app/alerts/components/alert-details/alert-details.component.ts
+++ frontend/src/app/alerts/components/alert-details/alert-details.component.ts
@@ -289,7 +289,7 @@ export class AlertDetailsComponent implements OnInit, OnDestroy, AfterContentIni
     startTime;
     endTime;
     timeZone = 'local';
-    autoRefresh = {'auto': 0, 'duration': 60 };
+    autoRefresh = {'auto': 0, 'duration': 0 };
     prevTimeSampler = null;
     downsample = { aggregators: [''], customUnit: '', customValue: '', value: 'auto'};
     prevDateRange: any = null;
